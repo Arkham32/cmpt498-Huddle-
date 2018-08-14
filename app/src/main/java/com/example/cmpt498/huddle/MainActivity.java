e099e9e595ae40e4f68e3dc823545bb6bfd3292d
tahmid <tahmid808@gmail.com>
2018-08-14 15:35:39 -0600

    the login pagees are done

--- app/src/main/java/com/example/cmpt498/huddle/MainActivity.java
+++ app/src/main/java/com/example/cmpt498/huddle/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cmpt498.huddle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToPassword(View view){
+
+        //grabs the email from the user
+        EditText userText = findViewById(R.id.LoginEmail);
+        String userEmail = userText.getText().toString();
+
+        //takes the inputs and goes to the password section of the login
+        Intent intent = new Intent(this, PasswordActivity.class);
+        intent.putExtra("EXTRA_EMAIL",userEmail);
+        startActivity(intent);
+
     }
 }
