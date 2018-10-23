dba1fdec78f096ead5d76cd551aa7ca634f967f8
tahmid <tahmid808@gmail.com>
2018-10-23 15:37:42 -0600

    almost done view post

--- app/src/main/java/com/example/raahymasif/cmpt498/FindMatchActivity.java
+++ app/src/main/java/com/example/raahymasif/cmpt498/FindMatchActivity.java
@@ -5,9 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.raahymasif.cmpt498.Model.CreatePosts;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 
 public class FindMatchActivity extends AppCompatActivity {
 
@@ -31,6 +35,48 @@ public class FindMatchActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        FirebaseRecyclerAdapter<CreatePosts,PostViewHolder>firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<CreatePosts,PostViewHolder>
+                (CreatePosts.class,R.layout.post_row,PostViewHolder.class,mDatabase){
+            @Override
+            protected void populateViewHolder(PostViewHolder viewHolder, CreatePosts model, int position) {
+                viewHolder.setInfo(model.getInfo());
+                viewHolder.setLocation(model.getLocation());
+                viewHolder.setPlayers(model.getPlayers());
+                viewHolder.setSport(model.getSport());
+            }
 
+        };
+
+        mPostList.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+        public PostViewHolder(View itemView){
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setInfo(String info){
+            TextView post_info = (TextView)mView.findViewById(R.id.post_info);
+            post_info.setText(info);
+        }
+
+        public void setLocation(String location){
+            TextView post_location = (TextView)mView.findViewById(R.id.post_location);
+            post_location.setText(location);
+        }
+
+        public void setPlayers(String players){
+            TextView post_players = (TextView)mView.findViewById(R.id.post_players);
+            post_players.setText(players);
+        }
+
+        public void setSport(String sport){
+            TextView post_sport = (TextView)mView.findViewById(R.id.post_sport);
+            post_sport.setText(sport);
+        }
     }
 }
--- app/src/main/java/com/example/raahymasif/cmpt498/FindMatchFragment.java
+++ app/src/main/java/com/example/raahymasif/cmpt498/FindMatchFragment.java
@@ -5,20 +5,95 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.example.raahymasif.cmpt498.Model.CreatePosts;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
 
 public class FindMatchFragment extends Fragment {
+    //---------------
+    /*private RecyclerView mPostList;
+    private DatabaseReference mDatabase;
+    //----------------
+    */
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return inflater.inflate(R.layout.fragment_findmatch, container, false);
         View view = inflater.inflate(R.layout.fragment_findmatch, container, false);
 
-        Intent findMatch = new Intent(getActivity(), FindMatchActivity.class);
-        startActivity(findMatch);
+        /*Intent findMatch = new Intent(getActivity(), FindMatchActivity.class);
+        startActivity(findMatch);*/
 
+        /*//-------------------------------------------
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_findmatch);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Posts");
+        mDatabase.keepSynced(true);
+
+        mPostList = (RecyclerView)findViewById(R.id.myRecycleView);
+        mPostList.setHasFixedSize(true);
+        mPostList.setLayoutManager(new LinearLayoutManager(this));
+        //-----------------------------------------------*/
         return view;
     }
+
+    /*//------------------------------
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<CreatePosts,FindMatchActivity.PostViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<CreatePosts,FindMatchActivity.PostViewHolder>
+                (CreatePosts.class,R.layout.post_row,FindMatchActivity.PostViewHolder.class,mDatabase){
+            @Override
+            protected void populateViewHolder(FindMatchActivity.PostViewHolder viewHolder, CreatePosts model, int position) {
+                viewHolder.setInfo(model.getInfo());
+                viewHolder.setLocation(model.getLocation());
+                viewHolder.setPlayers(model.getPlayers());
+                viewHolder.setSport(model.getSport());
+            }
+
+        };
+
+        mPostList.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+        public PostViewHolder(View itemView){
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setInfo(String info){
+            TextView post_info = (TextView)mView.findViewById(R.id.post_info);
+            post_info.setText(info);
+        }
+
+        public void setLocation(String location){
+            TextView post_location = (TextView)mView.findViewById(R.id.post_location);
+            post_location.setText(location);
+        }
+
+        public void setPlayers(String players){
+            TextView post_players = (TextView)mView.findViewById(R.id.post_players);
+            post_players.setText(players);
+        }
+
+        public void setSport(String sport){
+            TextView post_sport = (TextView)mView.findViewById(R.id.post_sport);
+            post_sport.setText(sport);
+        }
+    }
+}
+ //----------------------*/
 }
